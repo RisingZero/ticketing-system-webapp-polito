ec2a58f6a9bf6303612c5003eea238db05e3339b
RisingZero <ar.angelo259@gmail.com>
2024-06-19 10:57:45 +0200

    added morgan logging

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
 
 // init express
 const app = new express();
 const port = 3001;
+
+// use morgan for logging
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 // activate the server
 app.listen(port, () => {
