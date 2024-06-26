278adebcc8ea64e498920f52626ea34c3337dceb
RisingZero <ar.angelo259@gmail.com>
2024-06-26 22:31:21 +0200

    provideDbContext middleware

---
+++ server/middlewares/dbContext.js
@@ -0,0 +1,19 @@
+const DbService = require('../services/db.service');
+
+const provideDbContext = async (req, res, next) => {
+    const dbContext = new DbService();
+    try {
+        await dbContext.connect();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Error while connecting to the database',
+        });
+        return;
+    }
+    req.dbContext = dbContext;
+
+    next();
+};
+
+module.exports = provideDbContext;
