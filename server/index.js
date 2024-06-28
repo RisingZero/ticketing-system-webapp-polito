cbdfcc096d9662d61ec83bad7169293b4fff0cd4
RisingZero <ar.angelo259@gmail.com>
2024-06-28 12:28:13 +0200

    changed cors origin port

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // enable CORS
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:5173',
         methods: 'GET,POST,PUT,DELETE',
         credentials: true,
     })
--- server2/index.js
+++ server2/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // enable CORS
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:5173',
         methods: 'POST',
         credentials: true,
     })
