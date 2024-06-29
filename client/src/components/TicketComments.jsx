e873606a724cdf9197c08fc2ba44454f47ba4e3f
RisingZero <ar.angelo259@gmail.com>
2024-06-29 20:40:55 +0200

    client/ui and logic for ticket details and comments

---
+++ client/src/components/TicketComments.jsx
@@ -0,0 +1,249 @@
+import React from 'react';
+import AuthContext from '../AuthContext';
+import * as utils from '../utils';
+import { useToast } from '../hooks';
+import { ToastSeverity } from './Toast';
+import API from '../API';
+
+import {
+    Divider,
+    Sheet,
+    Box,
+    Stack,
+    Typography,
+    Textarea,
+    IconButton,
+    CircularProgress,
+} from '@mui/joy';
+import SendIcon from '@mui/icons-material/Send';
+
+function TicketComments({ ticketId, ticketStatus, sx }) {
+    const { addToast } = useToast();
+
+    const [loading, setLoading] = React.useState(true);
+    const [comments, setComments] = React.useState([]);
+    const commentsListRef = React.useRef();
+
+    const fetchComments = React.useCallback(() => {
+        API.getComments(ticketId)
+            .then((comments) => setComments(comments))
+            .catch((error) => {
+                addToast(error.message, { severity: ToastSeverity.ERROR });
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    React.useEffect(() => {
+        if (commentsListRef.current) {
+            commentsListRef.current.scrollTop =
+                commentsListRef.current.scrollHeight;
+        }
+    }, [comments]);
+
+    React.useEffect(() => {
+        fetchComments();
+    }, []);
+
+    return (
+        <Sheet
+            sx={{
+                ...sx,
+                borderRadius: 'xl',
+                p: 2,
+                minHeight: 200,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+            }}
+            variant="outlined"
+        >
+            {loading && comments.length === 0 && (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100%',
+                    }}
+                >
+                    <CircularProgress />
+                </Box>
+            )}
+            {!loading && comments.length === 0 && (
+                <Typography
+                    variant="caption"
+                    sx={{
+                        textAlign: 'center',
+                        color: 'text.secondary',
+                    }}
+                >
+                    No comments yet
+                </Typography>
+            )}
+            {comments.length > 0 && (
+                <>
+                    <Stack
+                        spacing={2}
+                        sx={{
+                            position: 'relative',
+                            maxHeight: 350,
+                            overflowY: 'auto',
+                            overflowAnchor: 'bottom',
+                            scrollBehavior: 'smooth',
+                        }}
+                        ref={commentsListRef}
+                    >
+                        {comments.map((comment) => (
+                            <CommentBubble comment={comment} />
+                        ))}
+                    </Stack>
+                    <Divider sx={{ my: 2 }} />
+                </>
+            )}
+            <CommentInput
+                ticketId={ticketId}
+                ticketStatus={ticketStatus}
+                onSubmit={fetchComments}
+                loadingComments={loading}
+            />
+        </Sheet>
+    );
+}
+
+function CommentBubble({ comment }) {
+    const auth = React.useContext(AuthContext);
+
+    const meAuthor = comment.authorId === auth.user.id;
+
+    return (
+        <Box
+            sx={[
+                {
+                    minWidth: '60%',
+                    maxWidth: '80%',
+                    alignSelf: meAuthor ? 'flex-end' : 'flex-start',
+                },
+            ]}
+        >
+            <Stack
+                direction="row"
+                justifyContent={'space-between'}
+                sx={{
+                    px: 0.5,
+                }}
+            >
+                <Typography level="title-sm">
+                    {comment.authorUsername}
+                </Typography>
+                <Typography variant="caption" fontSize={11}>
+                    {utils.formatDateFromEpoch(comment.postedAt)}
+                </Typography>
+            </Stack>
+            <Sheet
+                variant="soft"
+                sx={[
+                    {
+                        px: 1.75,
+                        py: 1.25,
+                        borderRadius: 'xl',
+                        whiteSpace: 'pre-wrap',
+                    },
+                    meAuthor
+                        ? { borderTopRightRadius: 0 }
+                        : { borderTopRightRadius: 'xl' },
+                    meAuthor
+                        ? { borderTopLeftRadius: 'xl' }
+                        : { borderTopLeftRadius: 0 },
+                ]}
+                color={meAuthor ? 'primary' : 'neutral'}
+            >
+                {comment.content}
+            </Sheet>
+        </Box>
+    );
+}
+
+function CommentInput({
+    ticketId,
+    ticketStatus,
+    onSubmit,
+    loadingComments,
+    sx,
+}) {
+    const { addToast } = useToast();
+
+    const [loading, setLoading] = React.useState(false);
+    const [content, setContent] = React.useState('');
+
+    const noCommentAllowed = ticketStatus === 'closed';
+
+    const submitComment = React.useCallback((content) => {
+        if (content.trim().length === 0) return;
+        setLoading(true);
+        API.createComment(ticketId, content.trim())
+            .then(() => {
+                setContent('');
+                if (onSubmit) onSubmit();
+            })
+            .catch((error) => {
+                addToast(error.message, { severity: ToastSeverity.ERROR });
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    return (
+        <Stack
+            direction="row"
+            spacing={1}
+            alignItems={'flex-end'}
+            sx={{ ...sx }}
+        >
+            <Textarea
+                minRows={2}
+                value={content}
+                sx={{
+                    borderRadius: 'md',
+                    width: '100%',
+                }}
+                placeholder={
+                    noCommentAllowed
+                        ? 'Ticket is closed, no more comments are allowed!'
+                        : 'Add a comment'
+                }
+                onChange={(e) => {
+                    setContent(e.target.value.slice(0, 1000));
+                }}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter' && !e.shiftKey) {
+                        e.preventDefault();
+                        // submit comment
+                        submitComment(content);
+                    }
+                }}
+                endDecorator={
+                    !noCommentAllowed && (
+                        <Typography level="body-xs" sx={{ ml: 'auto' }}>
+                            {content.length}/1000
+                        </Typography>
+                    )
+                }
+                readOnly={noCommentAllowed || loading}
+                color={noCommentAllowed ? 'warning' : 'neutral'}
+                variant={noCommentAllowed ? 'soft' : 'outlined'}
+            />
+            <IconButton
+                color="primary"
+                loading={loading || loadingComments}
+                variant="soft"
+                disabled={
+                    content.trim().length === 0 || ticketStatus === 'closed'
+                }
+                onClick={submitComment}
+            >
+                <SendIcon />
+            </IconButton>
+        </Stack>
+    );
+}
+
+export default TicketComments;
--- client/src/components/TicketDetails.jsx
+++ client/src/components/TicketDetails.jsx
@@ -1,17 +1,102 @@
 import React, { useState, useEffect } from 'react';
+import { ToastSeverity } from './Toast';
+import { useToast } from '../hooks';
 import API from '../API';
 
+import {
+    Sheet,
+    Divider,
+    Chip,
+    CircularProgress,
+    Box,
+    Stack,
+    Typography,
+} from '@mui/joy';
+import TicketComments from './TicketComments';
+
 function TicketDetails({ ticketId }) {
+    const { addToast } = useToast();
+
+    const [loading, setLoading] = useState(true);
+    const [ticket, setTicket] = useState(null);
+
+    const fetchData = () => {
+        setLoading(true);
+        API.getTicketById(ticketId)
+            .then((ticket) => {
+                setTicket(ticket);
+            })
+            .catch((error) => {
+                addToast(error.message, ToastSeverity.ERROR);
+            })
+            .finally(() => setLoading(false));
+    };
+
     useEffect(() => {
-        API.getTicketById(ticketId).then((data) => {
-            console.log(data);
-        });
-    }, [ticketId]);
+        fetchData();
+    }, []);
 
     return (
-        <div>
-            <h1>TicketDetails</h1>
-        </div>
+        <Sheet
+            sx={{
+                py: 2,
+            }}
+        >
+            {loading && (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100%',
+                    }}
+                >
+                    <CircularProgress />
+                </Box>
+            )}
+            {!loading && (
+                <Stack direction="row" spacing={2}>
+                    <Box
+                        sx={{
+                            width: '100%',
+                        }}
+                    >
+                        <Typography level="h3" sx={{ mb: 2 }}>
+                            {ticket.title}
+                        </Typography>
+                        <Stack direction="row" spacing={2}>
+                            <Typography
+                                color={
+                                    ticket.status === 'open'
+                                        ? 'success'
+                                        : 'danger'
+                                }
+                            >
+                                {ticket.status}
+                            </Typography>
+                            <Chip color="neutral" variant="outlined">
+                                {ticket.category}
+                            </Chip>
+                        </Stack>
+                        <Typography>
+                            Owned by <i>{ticket.ownerUsername}</i>
+                        </Typography>
+                        <Divider sx={{ my: 2, width: '80%' }} />
+                        <Typography level="title-md">Description</Typography>
+                        <Typography sx={{ whiteSpace: 'pre-wrap' }}>
+                            {ticket.description}
+                        </Typography>
+                    </Box>
+                    <TicketComments
+                        ticketId={ticketId}
+                        ticketStatus={ticket.status}
+                        sx={{
+                            width: '100%',
+                        }}
+                    />
+                </Stack>
+            )}
+        </Sheet>
     );
 }
 
