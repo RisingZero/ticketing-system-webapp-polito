01eb5100d03a16ec2420c1c1296e433c0638380c
RisingZero <ar.angelo259@gmail.com>
2024-06-30 19:04:59 +0200

    changed secrets

--- server/environment.js
+++ server/environment.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const env = {
-    SESSION_SECRET: 'sessionsecret',
-    JWT_SECRET: 'jwtsecret',
+    SESSION_SECRET: '392e09206c700eb15191b5a4ced0ef322eb8d258f9c11cb7b0ef61995c4ba0ee',
+    JWT_SECRET: '71e439416aadf71164e890872b75b70bd2923132848bdcd58eca0a9771375800',
     JWT_EXPIRY: 2 * 60,
 };
 
--- server2/environment.js
+++ server2/environment.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const env = {
-    JWT_SECRET: 'jwtsecret'
+    JWT_SECRET: '71e439416aadf71164e890872b75b70bd2923132848bdcd58eca0a9771375800'
 };
 
 module.exports = env;
